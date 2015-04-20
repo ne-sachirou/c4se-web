8106a351ff89a01d52c6f4cce314050a13c08a4f
ne_Sachirou <utakata.c4se@gmail.com>
2015-04-21 01:40:15 +0900

    メニュー開閉時にメインも動くやうに。メニューがメインに被さらないやうに。

--- src/javascripts/layout.js
+++ src/javascripts/layout.js
@@ -2,6 +2,8 @@
 
 class UiMenu {
   constructor() {
+    this.mainContent   = document.querySelector('.main_content');
+    this.mainFooter    = document.querySelector('.main_footer');
     this.menu          = document.querySelector('.main_menu');
     this.menuToggleBtn = this.menu.querySelector('.main_menu_toggleBtn');
     this.isOpen        = false;
@@ -15,14 +17,18 @@ class UiMenu {
   }
 
   open() {
-    this.isOpen = true;
-    this.menu.style.left = 0;
+    this.isOpen                        = true;
+    this.mainContent.style.left        = '60mm';
+    this.mainFooter.style.left         = '60mm';
+    this.menu.style.left               = 0;
     this.menuToggleBtn.style.transform = 'rotate(180deg)';
   }
 
   close() {
-    this.isOpen = false;
-    this.menu.style.left = '-48mm';
+    this.isOpen                        = false;
+    this.mainContent.style.left        = '12mm';
+    this.mainFooter.style.left         = '12mm';
+    this.menu.style.left               = '-48mm';
     this.menuToggleBtn.style.transform = 'rotate(0)';
   }
 }
