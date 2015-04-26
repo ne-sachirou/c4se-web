34a80e4222e3d2768fed28f243e69513f8fb96a3
ne_Sachirou <utakata.c4se@gmail.com>
2015-04-26 17:25:11 +0900

    Write 橘榛名のprofile.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$filename = __DIR__.preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
+$filename = __DIR__.preg_replace('#(\?.*)$#', '', urldecode($_SERVER['REQUEST_URI']));
 if (PHP_SAPI === 'cli-server' && is_file($filename)) {
     return false;
 }
