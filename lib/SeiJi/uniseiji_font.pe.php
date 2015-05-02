89eb31cca22e7ca44571253aea7d1e596db40c3a
ne_Sachirou <utakata.c4se@gmail.com>
2015-05-03 03:47:00 +0900

    Use FontForge script while() instead of PHP foreach().

--- lib/SeiJi/uniseiji_font.pe.php
+++ lib/SeiJi/uniseiji_font.pe.php
@@ -4,28 +4,39 @@ function uOrd($char)
     return unpack('N', mb_convert_encoding($char, 'UCS-4BE', 'UTF-8'))[1];
 }
 
-$chars  = explode(' ', trim(fgets(STDIN)));
-$charsO = $chars;
-$chars  = array_map(
+$chars     = explode(' ', trim(fgets(STDIN)));
+$charsOrig = $chars;
+$chars     = array_map(
     function ($char) {
         return '0u'.dechex(uOrd($char));
     },
     $chars
 );
+$charsOrig = array_map(
+    function ($char) {
+        return '"'.$char.'"';
+    },
+    $charsOrig
+)
 ?>#!/usr/bin/env fontforge
 
 Open('../../src/fonts/NotoSansCJKjp-Regular.otf')
 # CIDChangeSubFont('NotoSansCJKjp-Regular-Ideographs')
 CIDFlatten()
 Reencode('unicode')
-<?php foreach ($chars as $i => $char) { ?>
-if (InFont(<?php echo $char; ?>))
-    Print("<?php echo $charsO[$i]; ?>")
-    SelectMoreSingletons(<?php echo $char; ?>)
-else
-    Print("No <?php echo $charsO[$i]; ?>")
-endif
-<?php } ?>
+chars     = [<?php echo implode(',', $chars    ); ?>]
+charsOrig = [<?php echo implode(',', $charsOrig); ?>]
+i         = 0
+iz        = SizeOf(chars)
+while (i < iz)
+    if (InFont(chars[i]))
+        Print(charsOrig[i])
+        SelectMoreSingletons(chars[i])
+    else
+        Print("No " + charsOrig[i])
+    endif
+    ++i
+endloop
 SelectInvert()
 Clear()
 SetFondName('UniSeiJi')
