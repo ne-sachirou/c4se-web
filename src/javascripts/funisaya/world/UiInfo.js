52293b3da51a5ff319edd1b6083472dba57a4b67
ne_Sachirou <utakata.c4se@gmail.com>
2015-09-27 11:29:57 +0900

    blur

--- src/javascripts/funisaya/world/UiInfo.js
+++ src/javascripts/funisaya/world/UiInfo.js
@@ -2,27 +2,32 @@ import {EventRouter} from '../../_baselib.js';
 
 export class UiInfo {
   constructor() {
-    this._node = document.querySelector('.info');
+    this._node   = document.querySelector('.info');
+    this._nodeBg = document.querySelector('.infoBg');
     new UiInfoToggleBtn(this);
   }
 
   openInfo() {
     this._node.classList.remove('info-closed');
     this._node.classList.add('info-open');
+    this._nodeBg.classList.remove('infoBg-closed');
+    this._nodeBg.classList.add('infoBg-open');
   }
 
   closeInfo() {
     this._node.classList.remove('info-open');
     this._node.classList.add('info-closed');
+    this._nodeBg.classList.remove('infoBg-open');
+    this._nodeBg.classList.add('infoBg-closed');
   }
 }
 
 class UiInfoToggleBtn {
   constructor(parentUi) {
-    this._node       = document.querySelector('.infoToggleBtn');
-    this._nodeInner  = this._node.querySelector('.infoToggleBtn_inner');
-    this._uiInfo     = parentUi;
-    this._isInfoOpen = false;
+    this._node        = document.querySelector('.infoToggleBtn');
+    this._nodeInner   = this._node.querySelector('.infoToggleBtn_inner');
+    this._uiInfo      = parentUi;
+    this._isInfoOpen  = false;
     this._node.addEventListener('click', () => {
       if (this._isInfoOpen) {
         this._close();
